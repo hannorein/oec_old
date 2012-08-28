9f2fdb03ae78763b21f7abb0ad663fb176f3ee46
Hanno Rein <hanno@hanno-rein.de>
2012-08-27 21:50:15 -0400

    restructure

---
+++ data_images/scripts/resizeImages.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -v ../*.iPhone*
+rm -v ../*.iPad*
+rm -v ../*.web*
+
+
+for f in ../*.png
+do
+	filename=${f%.*}
+	echo $filename
+	convert -resize x260 -resize '960x<' -resize 50% -gravity center -crop 480x130+0+0 +repage -quality 90 "$filename.png" "$filename.iPhone.png"
+	convert -resize x260 -resize '2048x<' -resize 50% -gravity center -crop 1024x130+0+0 +repage -quality 90 "$filename.png" "$filename.iPad.png"
+	convert -resize x400 -resize '1200x<' -resize 50% -gravity center -crop 600x200+0+0 +repage -quality 90 "$filename.png" "$filename.web.png"
+	convert -resize 800x600 -quality 90 "$filename.png" "$filename.weblarger.png"
+done
+
